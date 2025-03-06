600f386c5e1ec008ca878b10f65b5aa66f410d9a
DanielAlmeidaSC <eexpeditodaniel@gmail.com>
2025-03-06 15:08:02 -0300

    adição de eventos

--- app.js
+++ app.js
@@ -1,38 +1,63 @@
-function black(){
-    var escuro = window.document.body.style.backgroundColor = "#222222";
-    var letrasClaras = window.document.body.style.color = "white";
-    var links = window.document.getElementsByClassName("text_decoration");
+
+let contadorClick = 0;
+
+const botaoModoDark = document.getElementById('modoEscuro');
+botaoModoDark.addEventListener('click', function() {
+    contadorClick++; // Incrementa o contador a cada clique
+
+    if (contadorClick === 1) {
+        black(); 
+    } else if (contadorClick === 2) {
+        // Recarrega a página no segundo clique, mantendo o modo escuro
+        location.reload();
+    }
+});
+
+
+function black() {
+    var titleEscuro = document.getElementById('modoEscuro');
+    titleEscuro.innerHTML = "Modo Claro";
+    var links = document.getElementsByClassName("text_decoration");
     for (var i = 0; i < links.length; i++) {
         links[i].style.color = "white";
     }
 
-    var redes = window.document.getElementsByClassName("botao-click");
-    var negrito = window.document.getElementByTagName("strong");
-    negrito.body.style.color = "white";
+    var tituloSobreEu = document.getElementById('titleAboutMe');
+    if (tituloSobreEu) {
+        tituloSobreEu.style.color = "gold";
+    }
 
+    document.body.style.backgroundColor = "#222222";
+    document.body.style.color = "white";
 
+    var negrito = document.getElementById('negrito');
+    if (negrito) {
+        negrito.style.color = "gold";
+    }
+    var redes = document.getElementsByClassName("botao-click");
     for (var i = 0; i < redes.length; i++) {
         redes[i].style.color = "white";
-        redes[i].style.backgroundColor = "#222222";
-        redes[i].style.borderColor = "white";
+        redes[i].style.border = "1px solid white";
+        redes[i].style.boxShadow = "2px 2px 10px gold";
+        redes[i].style.backgroundColor = "transparent";
     }
 
-    var rodape = window.document.getElementsByClassName("rodape");
+    var rodape = document.querySelectorAll('footer');
     for (var i = 0; i < rodape.length; i++) {
-        rodape[i].style.backgroundColor = "white";
+        rodape[i].style.backgroundColor = "black";
     }
 
-    var textoRodape = window.document.getElementById("textoRodape");
-    textoRodape.style.color = "black";
-
-    var tituloSobreEu = window.document.getElementById("titleAboutMe");
-    tituloSobreEu.style.color = "white";
-
-        var buttonWhats = window.document.getElementsByClassName("text_redes");
-        for (var i = 0; i < buttonWhats.length; i++) {
-            buttonWhats[i].style.color = "white";  // Altera a cor do texto para branco
-        
+    var textoRodape = document.getElementsByClassName('textoRodape');
+        for (var i = 0; i < textoRodape.length; i++) {
+            textoRodape[i].style.color = "gold";
         }
 
-    return escuro;
+    var whatsapp = document.getElementsByClassName('botoes_contato1');
+    if(whatsapp){
+        whatsapp[0].style.backgroundColor = "transparent";
+        whatsapp[0].style.border = "1px solid white";
+        
+        var whatsWord = document.getElementsByClassName('text_redes');
+        whatsWord[0].style.color = "white";
+    }
 }
